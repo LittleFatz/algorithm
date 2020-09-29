534dfd38a6cfdbb3305f7ac81bfeb8cd48708e2b
littlefatz <littlefatz@qq.com>
2020-09-29 21:04:17 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/dp/CoinChange.java
+++ leetcode/src/main/java/com/littlefatz/dp/CoinChange.java
@@ -4,48 +4,48 @@ import java.util.Arrays;
 
 //https://leetcode.com/problems/coin-change/description/
 public class CoinChange {
-
-    public int coinChange(int[] coins, int amount) {
-
-        int[] dp = new int[amount + 1];
-        int coinLength = coins.length;
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-
-            dp[i] = Integer.MAX_VALUE;
-            for (int j = 0; j < coinLength; j++) {
-                int leftAmount = i - coins[j];
-                if (leftAmount >= 0 && dp[leftAmount] >= 0) {
-                    dp[i] = Math.min(dp[i], dp[leftAmount] + 1);
-                }
-            }
-            if (dp[i] == Integer.MAX_VALUE) {
-                dp[i] = -1;
-            }
-
-        }
-
-        return dp[amount];
-
-    }
-
-    //参考答案
-    public class Solution {
-        public int coinChange(int[] coins, int amount) {
-            int max = amount + 1;
-            int[] dp = new int[amount + 1];
-            Arrays.fill(dp, max);
-            dp[0] = 0;
-            for (int i = 1; i <= amount; i++) {
-                for (int j = 0; j < coins.length; j++) {
-                    if (coins[j] <= i) {
-                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                    }
-                }
-            }
-            return dp[amount] > amount ? -1 : dp[amount];
-        }
-    }
+//
+//    public int coinChange(int[] coins, int amount) {
+//
+//        int[] dp = new int[amount + 1];
+//        int coinLength = coins.length;
+//        dp[0] = 0;
+//        for (int i = 1; i <= amount; i++) {
+//
+//            dp[i] = Integer.MAX_VALUE;
+//            for (int j = 0; j < coinLength; j++) {
+//                int leftAmount = i - coins[j];
+//                if (leftAmount >= 0 && dp[leftAmount] >= 0) {
+//                    dp[i] = Math.min(dp[i], dp[leftAmount] + 1);
+//                }
+//            }
+//            if (dp[i] == Integer.MAX_VALUE) {
+//                dp[i] = -1;
+//            }
+//
+//        }
+//
+//        return dp[amount];
+//
+//    }
+//
+//    //参考答案
+//    public class Solution {
+//        public int coinChange(int[] coins, int amount) {
+//            int max = amount + 1;
+//            int[] dp = new int[amount + 1];
+//            Arrays.fill(dp, max);
+//            dp[0] = 0;
+//            for (int i = 1; i <= amount; i++) {
+//                for (int j = 0; j < coins.length; j++) {
+//                    if (coins[j] <= i) {
+//                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+//                    }
+//                }
+//            }
+//            return dp[amount] > amount ? -1 : dp[amount];
+//        }
+//    }
 
 
     public static void main(String[] args) {
@@ -54,5 +54,22 @@ public class CoinChange {
 
 
         System.out.println(test.coinChange(coins,27));
+    }
+
+    public int coinChange(int[] coins, int amount) {
+
+        int length = coins.length;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
