4f26c064a8ef9ca95dd632a95c15485fe834a835
littlefatz <littlefatz@qq.com>
2020-10-27 20:23:11 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/RemovedDuplicatesFromSortedArray.java
+++ leetcode/src/main/java/com/littlefatz/RemovedDuplicatesFromSortedArray.java
@@ -63,7 +63,7 @@ public class RemovedDuplicatesFromSortedArray {
 
     }
 
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicates4(int[] nums) {
         int length = nums.length;
 
         int slow = 0;
@@ -77,5 +77,24 @@ public class RemovedDuplicatesFromSortedArray {
         }
 
         return slow+1;
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        if (length == 0) {
+            return 0;
+        }
+
+        int slow = 0;
+        int fast = 0;
+        while (fast < length) {
+            if (nums[slow] != nums[fast]) {
+                nums[++slow] = nums[fast];
+            }
+
+            fast++;
+        }
+
+        return slow + 1;
     }
 }
