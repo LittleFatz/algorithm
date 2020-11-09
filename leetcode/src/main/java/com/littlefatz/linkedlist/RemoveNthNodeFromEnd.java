3bc5108ad75b28e5edbf56a4a82dc23ccc4ac599
littlefatz <littlefatz@qq.com>
2020-11-09 21:09:52 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/linkedlist/RemoveNthNodeFromEnd.java
+++ leetcode/src/main/java/com/littlefatz/linkedlist/RemoveNthNodeFromEnd.java
@@ -2,10 +2,12 @@ package com.littlefatz.linkedlist;
 
 import com.littlefatz.ListNode;
 
+import java.util.List;
+
 //https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
 public class RemoveNthNodeFromEnd {
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
 
         ListNode result = new ListNode(0);
         result.next = head;
@@ -27,4 +29,48 @@ public class RemoveNthNodeFromEnd {
 
         return result.next;
     }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        //按照题目意思，n 始终有效。因此如果需要删除的数字在 head 位置，那么 fast 会一直遍历到尾结点的 null
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+
+        // 如果此时快指针走到头了，
+        // 说明倒数第 n 个节点就是第一个节点
+        if (fast == null) {
+            return head.next;
+        }
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
