888667d81b566b1cd640bd745c1aceccbf84ccc6
littlefatz <littlefatz@qq.com>
2020-11-12 21:15:20 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/dp/IntegerBreak.java
+++ leetcode/src/main/java/com/littlefatz/dp/IntegerBreak.java
@@ -4,7 +4,7 @@ package com.littlefatz.dp;
 //https://leetcode-cn.com/problems/integer-break/
 public class IntegerBreak {
 
-    public int integerBreak(int n) {
+    public int integerBreak2(int n) {
 
         if (n <= 3) {
             return n;
@@ -24,5 +24,27 @@ public class IntegerBreak {
     public static void main(String[] args) {
         IntegerBreak test = new IntegerBreak();
         System.out.println(test.integerBreak(10));
+    }
+
+
+    public int integerBreak(int n) {
+
+        if (n <= 3) {
+            return n - 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+
+                /**
+                 * Math.max(dp[j], j)
+                 * 之所以这里要 max，是因为 2 和 3 这两个数组都是不用拆分的
+                 */
+                dp[i] = Math.max(dp[i], (i - j) * Math.max(dp[j], j));
+            }
+        }
+
+        return dp[n];
     }
 }
