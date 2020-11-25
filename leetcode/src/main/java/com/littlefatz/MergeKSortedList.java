ce6e0e964723fe324f3328aed627941c4b82108d
Stevie Huang <stevie.huang@ericsson.com>
2020-11-25 17:20:35 +0800

    merge sort

--- leetcode/src/main/java/com/littlefatz/MergeKSortedList.java
+++ leetcode/src/main/java/com/littlefatz/MergeKSortedList.java
@@ -7,27 +7,69 @@ public class MergeKSortedList {
      * 归并思想
      * 先把链表数组拆分，分治思想，然后再用归并排序的思想，两两合并链表，最后变成一条有序链表
      */
+//    public ListNode mergeKLists(ListNode[] lists) {
+//        if (lists == null || lists.length == 0) {
+//            return null;
+//        }
+//
+//        return merge(lists, 0, lists.length - 1);
+//    }
+//
+//    private ListNode merge(ListNode[] lists, int left, int right) {
+//        if (left == right) {
+//            return lists[left];
+//        }
+//
+//        int middle = (left + right) >> 1;
+//        ListNode list1 = merge(lists, left, middle);
+//        ListNode list2 = merge(lists, middle + 1, right);
+//
+//        return merger2List(list1, list2);
+//    }
+//
+//    private ListNode merger2List(ListNode list1, ListNode list2) {
+//        if (list1 == null) {
+//            return list2;
+//        }
+//
+//        if (list2 == null) {
+//            return list1;
+//        }
+//
+//        if (list1.val <= list2.val) {
+//            //精妙之处，一边归并，一边递归
+//            list1.next = merger2List(list1.next, list2);
+//            return list1;
+//        } else {
+//            list2.next = merger2List(list1, list2.next);
+//            return list2;
+//        }
+//
+//    }
+
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
             return null;
         }
-        
+
         return merge(lists, 0, lists.length - 1);
     }
 
-    private ListNode merge(ListNode[] lists, int left, int right) {
-        if (left == right) {
-            return lists[left];
+    private ListNode merge(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
         }
 
-        int middle = (left + right) >> 1;
-        ListNode list1 = merge(lists, left, middle);
-        ListNode list2 = merge(lists, middle + 1, right);
+        int middle = (start + end) / 2;
+        ListNode list1 = merge(lists, start, middle);
+        ListNode list2 = merge(lists, middle + 1, end);
 
-        return merger2Litst(list1, list2);
+        return mergeTwoLists(lists, list1, list2);
+
     }
 
-    private ListNode merger2Litst(ListNode list1, ListNode list2) {
+    private ListNode mergeTwoLists(ListNode[] lists, ListNode list1, ListNode list2) {
+
         if (list1 == null) {
             return list2;
         }
@@ -37,15 +79,13 @@ public class MergeKSortedList {
         }
 
         if (list1.val <= list2.val) {
-            //精妙之处，一边归并，一边递归
-            list1.next = merger2Litst(list1.next, list2);
+            list1.next = mergeTwoLists(lists, list1.next, list2);
             return list1;
         } else {
-            list2.next = merger2Litst(list1, list2.next);
+            list2.next = mergeTwoLists(lists, list1, list2.next);
             return list2;
         }
 
+
     }
-
-
 }
