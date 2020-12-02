2c8e15ec2030cda670d3179f01a0b5119c6df224
Stevie Huang <stevie.huang@ericsson.com>
2020-12-02 17:49:06 +0800

    merge sort

--- leetcode/src/main/java/com/littlefatz/binarysearch/FindFirstAndLastPostionOfElement.java
+++ leetcode/src/main/java/com/littlefatz/binarysearch/FindFirstAndLastPostionOfElement.java
@@ -4,20 +4,20 @@ package com.littlefatz.binarysearch;
 //题解参考：https://mp.weixin.qq.com/s/M1KfTfNlu4OCK8i9PSAmug
 public class FindFirstAndLastPostionOfElement {
 
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange2(int[] nums, int target) {
 
         if (nums == null || nums.length == 0) {
             return new int[]{-1,-1};
         }
 
         int[] result = new int[2];
-        result[0] = findLeftBound(nums, target);
-        result[1] = findRightBound(nums, target);
+        result[0] = findLeftBound2(nums, target);
+        result[1] = findRightBound2(nums, target);
 
         return result;
     }
 
-    private int findLeftBound(int[] nums, int target) {
+    private int findLeftBound2(int[] nums, int target) {
 
         int left = 0;
         int right = nums.length - 1;
@@ -45,7 +45,7 @@ public class FindFirstAndLastPostionOfElement {
 
     }
 
-    private int findRightBound(int[] nums, int target) {
+    private int findRightBound2(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
 
@@ -60,11 +60,80 @@ public class FindFirstAndLastPostionOfElement {
             }
         }
 
-        //道理和上面一样，由于这个方法需要找到的是 rightBound，因此只需要关注 right 即可
+        /**
+         * 道理和上面一样，由于这个方法需要找到的是 rightBound，因此只需要关注 right 即可
+         * right < 0: target 比所有元素都小， right = -1
+         * nums[right] != target: target 比所有元素都大，left = right + 1
+         */
+        //
         if (right < 0 || nums[right] != target) {
             return -1;
         }
 
         return right;
     }
+
+    public int[] searchRange(int[] nums, int target) {
+        int length = nums.length;
+        if (length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int leftBound = findLeftBound(nums, target);
+        int rightBound = findRightBound(nums, target);
+        return new int[]{leftBound, rightBound};
+    }
+
+
+    private int findLeftBound(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        if (left >= nums.length || nums[left] != target) {
+            return -1;
+        }
+
+        return left;
+    }
+
+
+    private int findRightBound(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+
+        return right;
+
+    }
+
+
+
+
 }
