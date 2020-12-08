2ff14d9237c5fd3c3e59460bb6e2beb8a5f2f55c
littlefatz <littlefatz@qq.com>
2020-12-08 23:20:38 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/dfs/GenerateParentheses.java
+++ leetcode/src/main/java/com/littlefatz/dfs/GenerateParentheses.java
@@ -38,31 +38,60 @@ public class GenerateParentheses {
 
         GenerateParentheses test = new GenerateParentheses();
         test.generateParenthesis(3);
-        for (String s : test.results) {
+        for (String s : test.result) {
             System.out.println(s);
         }
     }
 
-    private List<String> results = new ArrayList<>();
+//    private List<String> results = new ArrayList<>();
+//
+//    public List<String> generateParenthesis(int n) {
+//        dfs(n, n, "");
+//        return results;
+//    }
+//
+//    private void dfs(int left, int right, String current) {
+//
+//        if (left == 0 && right == 0) {
+//            results.add(current);
+//            return;
+//        }
+//
+//        if (left > 0) {
+//            dfs(left - 1, right, current + "(");
+//        }
+//
+//        if (right > left) {
+//            dfs(left, right - 1, current + ")");
+//        }
+//
+//    }
 
     public List<String> generateParenthesis(int n) {
-        dfs(n, n, "");
-        return results;
+        generate(n, n, "");
+        return result;
     }
 
-    private void dfs(int left, int right, String current) {
+    private List<String> result = new ArrayList<>();
+
+    private void generate(int left, int right, String temp) {
 
         if (left == 0 && right == 0) {
-            results.add(current);
+            result.add(temp);
             return;
         }
 
         if (left > 0) {
-            dfs(left - 1, right, current + "(");
+            generate(left - 1, right, temp + "(");
         }
 
+        /**
+         * 只有当右括号剩余数大于左括号剩余数，才能往下递归，以下是两种无效情况：
+         * 1. left = right： 这时候不能先加右括号
+         * 2. right < left: 这时候无论怎样填，生成的括号都是无效
+         */
         if (right > left) {
-            dfs(left, right - 1, current + ")");
+            generate(left, right - 1, temp + ")");
         }
 
     }
