d7ca25bc1f5b77d30589aa0471769d9c09c305dd
Stevie Huang <stevie.huang@ericsson.com>
2020-12-10 10:43:27 +0800

    merge sort

--- leetcode/src/main/java/com/littlefatz/dp/BestTimeToBuySellStockIII.java
+++ leetcode/src/main/java/com/littlefatz/dp/BestTimeToBuySellStockIII.java
@@ -3,7 +3,7 @@ package com.littlefatz.dp;
 //https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/description/
 public class BestTimeToBuySellStockIII {
 
-    public int maxProfit(int[] prices) {
+    public int maxProfit2(int[] prices) {
         int dpI10 = 0;//第i天做了1次交易，没有持股的最大收益
 
         //第i天做了1次交易，正在持股的最大收益
@@ -20,10 +20,55 @@ public class BestTimeToBuySellStockIII {
             dpI20 = Math.max(dpI20, dpI21 + price);
             dpI21 = Math.max(dpI21, dpI10 - price);
             dpI10 = Math.max(dpI10, dpI11 + price);
+            /**
+             * 其实这里应该是 dpI11 = Math.max(dpI11, dpI00 - price);
+             * dpI00: 前一天没有做过任何交易，没有持股
+             * 由于 dpI00 只能从 dp[i-1][0][0] 推算过来的，而dp[0][0][0] 等于 0，因此所有的 dpI00 都是 0
+             */
             dpI11 = Math.max(dpI11, -price);
         }
 
         return dpI20;
 
     }
+
+    public int maxProfit(int[] prices) {
+
+        int length = prices.length;
+        if (length == 0) {
+            return 0;
+        }
+
+        int[][][] dp = new int[length][3][2];
+        dp[0][1][0] = 0;
+        dp[0][1][1] = -prices[0];
+        dp[0][2][0] = 0;
+        dp[0][2][1] = -prices[0];
+
+        for (int i = 1; i < length; i++) {
+            for (int k = 2; k >= 1; k--) {
+                dp[i][k][0] = Math.max(dp[i-1][k][0], dp[i-1][k][1] + prices[i]);
+                dp[i][k][1] = Math.max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i]);
+            }
+        }
+
+        return dp[length - 1][2][0];
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- leetcode/src/main/java/com/littlefatz/linkedlist/AddTwoNumbers.java
+++ leetcode/src/main/java/com/littlefatz/linkedlist/AddTwoNumbers.java
@@ -39,7 +39,7 @@ public class AddTwoNumbers {
 //
 //    }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
 
 
         ListNode result = new ListNode(0);
@@ -68,6 +68,42 @@ public class AddTwoNumbers {
 
     }
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        int carry = 0;
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int value1 = 0;
+            int value2 = 0;
+
+            if (l1 != null) {
+                value1 = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                value2 = l2.val;
+                l2 = l2.next;
+            }
+
+
+            int sum = value1 + value2 + carry;
+            int value = sum % 10;
+            carry = sum / 10;
+            current.next = new ListNode(value);
+            current = current.next;
+
+        }
+
+        return head.next;
+    }
 
 }
