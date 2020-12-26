95bb303f96e06402d43226ff09e0a7dfd12d61e3
littlefatz <littlefatz@qq.com>
2020-12-26 19:38:46 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/dp/LongestPalindromicSubsequence.java
+++ leetcode/src/main/java/com/littlefatz/dp/LongestPalindromicSubsequence.java
@@ -4,7 +4,7 @@ package com.littlefatz.dp;
 //https://leetcode-cn.com/problems/longest-palindromic-subsequence/
 public class LongestPalindromicSubsequence {
 
-    public int longestPalindromeSubseq(String s) {
+    public int longestPalindromeSubseq2(String s) {
 
         int length = s.length();
         if (length < 2) {
@@ -29,4 +29,41 @@ public class LongestPalindromicSubsequence {
 
         return dp[0][length-1];
     }
+
+    public int longestPalindromeSubseq(String s) {
+        int length = s.length();
+        if (length == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = 1;
+        }
+
+        char[] chars = s.toCharArray();
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                if (chars[i] == chars[j]) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i+1][j]);
+                }
+            }
+        }
+
+        return dp[0][length-1];
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
