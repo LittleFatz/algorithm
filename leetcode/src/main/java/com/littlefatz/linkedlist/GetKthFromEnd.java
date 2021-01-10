41557df015658c774c97902077828bf506c01aec
littlefatz <littlefatz@qq.com>
2021-01-10 10:14:57 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/linkedlist/GetKthFromEnd.java
+++ leetcode/src/main/java/com/littlefatz/linkedlist/GetKthFromEnd.java
@@ -2,10 +2,12 @@ package com.littlefatz.linkedlist;
 
 import com.littlefatz.ListNode;
 
+import java.util.List;
+
 //https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
 public class GetKthFromEnd {
 
-    public ListNode getKthFromEnd(ListNode head, int k) {
+    public ListNode getKthFromEnd2(ListNode head, int k) {
 
 
         ListNode slow = head;
@@ -25,4 +27,33 @@ public class GetKthFromEnd {
 
 
     }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        int count = k;
+        while (count > 0 && fast != null) {
+            count--;
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+
+
+
+
+
+
+
+
+
 }
