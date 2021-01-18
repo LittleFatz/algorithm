c6b67ad291919e6af526f6dd528315564b0ff48b
littlefatz <littlefatz@qq.com>
2021-01-18 20:35:11 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/Permutations.java
+++ leetcode/src/main/java/com/littlefatz/Permutations.java
@@ -8,11 +8,11 @@ import java.util.List;
 //回溯法，原理类似于深度优先遍历树，每次遍历到叶子节点后，需要回退到上一个状态
 public class Permutations {
 
-    private List<List<Integer>> results = new ArrayList<>();
-    private int size = 0;
-    private boolean[] used;
+//    private List<List<Integer>> results = new ArrayList<>();
+//    private int size = 0;
+//    private boolean[] used;
 
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permute2(int[] nums) {
         if (nums.length == 0) {
             return results;
         }
@@ -25,7 +25,7 @@ public class Permutations {
     }
 
 
-    private void backTrack(int[] nums, int depth, LinkedList<Integer> current) {
+    private void backTrack2(int[] nums, int depth, LinkedList<Integer> current) {
 
         if (depth == size) {
             results.add(new LinkedList<>(current));
@@ -50,5 +50,43 @@ public class Permutations {
         Permutations test = new Permutations();
         List<List<Integer>> results = test.permute(new int[]{1,2,3});
         System.out.println(results.toString());
+    }
+
+    private List<List<Integer>> result = new ArrayList<>();
+    private int size;
+    private boolean[] used;
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        int length = nums.length;
+        if (length == 0) {
+            return result;
+        }
+
+        size = length;
+        used = new boolean[size];
+        LinkedList<Integer> current = new LinkedList<>();
+        backTrack(nums, 0, current);
+        return result;
+
+    }
+
+    private void backTrack(int[] nums, int depth, LinkedList<Integer> current) {
+
+        if (depth == size) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (!used[i]) {
+                current.addLast(nums[i]);
+                used[i] = true;
+                backTrack(nums, depth + 1, current);
+                used[i] = false;
+                current.removeLast();
+            }
+        }
+
     }
 }
