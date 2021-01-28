ae334de5d697cfaf7eefca880cf9dc0f2f7afa2c
Stevie Huang <stevie.huang@ericsson.com>
2021-01-28 16:25:21 +0800

    merge sort

--- leetcode/src/main/java/com/littlefatz/dp/TaskScheduler.java
+++ leetcode/src/main/java/com/littlefatz/dp/TaskScheduler.java
@@ -3,9 +3,11 @@ package com.littlefatz.dp;
 import java.util.Arrays;
 
 //https://leetcode-cn.com/problems/task-scheduler/
+//题解：https://leetcode-cn.com/problems/task-scheduler/solution/tong-zi-by-popopop/
+//https://leetcode-cn.com/problems/task-scheduler/solution/tian-tong-si-lu-you-tu-kan-wan-jiu-dong-by-mei-jia/
 public class TaskScheduler {
 
-    public int leastInterval(char[] tasks, int n) {
+    public int leastInterval2(char[] tasks, int n) {
 
         int[] map = new int[26];
         for (char task : tasks) {
@@ -27,8 +29,40 @@ public class TaskScheduler {
         }
 
         /**
-         * idleSlots + tasks.length:
+         * （这个可以从图去观察）
+         * idleSlots > 0: 证明任务填不满 idleSlot，所以总时间等于 idleSlot + tasks.length
+         * idleSlots <= 0: 任务种类多，idleSlot 容不下这么多 task，因此部分桶需要扩大，实际消耗时间 = tasks.length
          */
         return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
     }
+
+    public int leastInterval(char[] tasks, int n) {
+
+        int[] bucket = new int[26];
+        for (char task : tasks) {
+            bucket[task - 'A']++;
+        }
+
+        Arrays.sort(bucket);
+        int maxTimes = bucket[25];
+        int idleSlots = (maxTimes - 1) * (n + 1);
+
+        for (int i = 24; i >= 0 && bucket[i] > 0; i--) {
+            idleSlots -= Math.min(maxTimes - 1, bucket[i]);
+        }
+
+        return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
