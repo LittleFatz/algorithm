e2865fc4de4ca60cc276c2a058353ea62e2aa15d
littlefatz <littlefatz@qq.com>
2021-01-30 19:10:29 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/linkedlist/MergeTwoSortedList.java
+++ leetcode/src/main/java/com/littlefatz/linkedlist/MergeTwoSortedList.java
@@ -10,7 +10,7 @@ import java.util.stream.IntStream;
 //https://leetcode-cn.com/problems/merge-two-sorted-lists/
 public class MergeTwoSortedList {
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode result = head;
 
@@ -61,4 +61,46 @@ public class MergeTwoSortedList {
             head = head.next;
         }
     }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode head = new ListNode(-1);
+        ListNode current = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (l1 != null) {
+            current.next = l1;
+        }
+
+        if (l2 != null) {
+            current.next = l2;
+        }
+
+        return head.next;
+    }
+
+
+
+
+
+
+
 }
