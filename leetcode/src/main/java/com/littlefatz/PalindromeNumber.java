17828c0f89da4d87d131aa1262bb7452ce01213e
littlefatz <littlefatz@qq.com>
2021-02-11 14:51:04 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/PalindromeNumber.java
+++ leetcode/src/main/java/com/littlefatz/PalindromeNumber.java
@@ -2,7 +2,7 @@ package com.littlefatz;
 
 //https://leetcode-cn.com/problems/palindrome-number/
 public class PalindromeNumber {
-    public boolean isPalindrome(int x) {
+    public boolean isPalindrome2(int x) {
         if (x < 0) {
             return false;
         }
@@ -19,4 +19,27 @@ public class PalindromeNumber {
 
 
     }
+
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int result = 0;
+        int number = x;
+        while (number > 0) {
+            int pop = number % 10;
+            result = result * 10 + pop;
+            number = number / 10;
+        }
+
+        return x == result;
+    }
+
+
+
+
+
+
 }
