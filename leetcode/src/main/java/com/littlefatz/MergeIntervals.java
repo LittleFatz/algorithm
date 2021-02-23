0b2f2b9ab77efded46a7fac7d63d4da5e2917315
littlefatz <littlefatz@qq.com>
2021-02-23 21:17:59 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/MergeIntervals.java
+++ leetcode/src/main/java/com/littlefatz/MergeIntervals.java
@@ -6,10 +6,11 @@ import java.util.Comparator;
 import java.util.List;
 
 //https://leetcode-cn.com/problems/merge-intervals/
+//https://leetcode-cn.com/problems/merge-intervals/solution/chi-jing-ran-yi-yan-miao-dong-by-sweetiee/
 public class MergeIntervals {
 
 
-    public int[][] merge(int[][] intervals) {
+    public int[][] merge2(int[][] intervals) {
         int length = intervals.length;
         if (length < 2) {
             return intervals;
@@ -30,5 +31,36 @@ public class MergeIntervals {
 
         return result.toArray(new int[result.size()][2]);
     }
+
+
+    public int[][] merge(int[][] intervals) {
+
+        int length = intervals.length;;
+        if (length < 2) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+
+        int[][] result = new int[length][2];
+        int index = -1;
+        for (int[] interval : intervals) {
+            if (index == -1 || result[index][1] < interval[0]) {
+                result[++index] = interval;
+            } else {
+                result[index][1] = Math.max(interval[1], result[index][1]);
+            }
+        }
+
+        return Arrays.copyOf(result, index + 1);
+
+    }
+
+
+
+
+
+
+
 
 }
