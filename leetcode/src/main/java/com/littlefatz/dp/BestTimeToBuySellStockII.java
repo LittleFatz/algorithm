8efa1f3eb8209f9f66df4ca5b0932c688e3d169e
littlefatz <littlefatz@qq.com>
2021-03-14 18:26:40 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/dp/BestTimeToBuySellStockII.java
+++ leetcode/src/main/java/com/littlefatz/dp/BestTimeToBuySellStockII.java
@@ -11,6 +11,13 @@ public class BestTimeToBuySellStockII {
         for (int i = 0; i < length; i++) {
             int temp = dp_i_0;
             dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            /**
+             * 注意和第一题的区别
+             * 这里需要计算上前一天不持有股票的最大收益
+             * 因为该题是无限交易次数，因此 dp_i_0 不一定为 0，前 i - 1 天可能做了很多次交易
+             * 所以这里买入股票的话，需要减去 prices[i]
+             *
+             */
             dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
         }
 
