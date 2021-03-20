3e71d6b1cf8ac70086a3ddc2d8411adb2887e3dd
littlefatz <littlefatz@qq.com>
2021-03-20 11:10:41 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/tree/MaxDepthOfBinaryTree.java
+++ leetcode/src/main/java/com/littlefatz/tree/MaxDepthOfBinaryTree.java
@@ -4,6 +4,15 @@ import com.littlefatz.tree.TreeNode;
 
 //https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
 public class MaxDepthOfBinaryTree {
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int depth = Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        return depth;
+    }
+
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
@@ -12,4 +21,14 @@ public class MaxDepthOfBinaryTree {
         int depth = Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
         return depth;
     }
+
+
+
+
+
+
+
+
+
+
 }
