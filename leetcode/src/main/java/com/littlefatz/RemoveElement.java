8004784b902849047a33feae4251ef7bfb41e367
littlefatz <littlefatz@qq.com>
2022-03-27 20:20:44 +0800

    commit

---
+++ leetcode/src/main/java/com/littlefatz/RemoveElement.java
@@ -0,0 +1,22 @@
+package com.littlefatz;
+
+//https://leetcode-cn.com/problems/remove-element/
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
