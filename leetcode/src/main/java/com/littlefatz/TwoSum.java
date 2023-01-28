b7c38b4e4a01b4d7357a2e92f18b91b87a6154d8
littlefatz <littlefatz@qq.com>
2023-01-28 14:02:44 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/TwoSum.java
+++ leetcode/src/main/java/com/littlefatz/TwoSum.java
@@ -8,18 +8,53 @@ import java.util.Map;
 //哈希表
 
 public class TwoSum {
+//    public int[] twoSum(int[] nums, int target) {
+//        Map<Integer, Integer> index = new HashMap<Integer, Integer>();
+//
+//        for (int i = 0; i < nums.length; i++) {
+//
+//            int complement = target - nums[i];
+//            if (index.containsKey(complement)) {
+//                return new int[]{index.get(complement), i};
+//            }
+//            index.put(nums[i], i);
+//
+//        }
+//        return new int[]{};
+//    }
+
+
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> index = new HashMap<Integer, Integer>();
 
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-
-            int complement = target - nums[i];
-            if (index.containsKey(complement)) {
-                return new int[]{index.get(complement), i};
+            int num = nums[i];
+            int diff = target - num;
+            if (map.containsKey(diff)) {
+                return new int[]{i, map.get(diff)};
             }
-            index.put(nums[i], i);
 
+            map.put(num, i);
         }
+
         return new int[]{};
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
