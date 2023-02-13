a7903f1e7aec5924b0d6bc73a0bcd42350845e96
littlefatz <littlefatz@qq.com>
2023-02-13 20:36:12 +0800

    commit

--- leetcode/src/main/java/com/littlefatz/linkedlist/MergeTwoSortedList.java
+++ leetcode/src/main/java/com/littlefatz/linkedlist/MergeTwoSortedList.java
@@ -137,7 +137,7 @@ public class MergeTwoSortedList {
     }
 
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    public ListNode mergeTwoLists5(ListNode list1, ListNode list2) {
 
         if (list1 == null) {
             return list2;
@@ -175,6 +175,46 @@ public class MergeTwoSortedList {
 
         return head.next;
     }
+
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode pre = new ListNode(0);
+        ListNode head = pre;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                pre.next = list1;
+                list1 = list1.next;
+            } else {
+                pre.next = list2;
+                list2 = list2.next;
+            }
+
+            pre = pre.next;
+        }
+
+        if (list1 != null) {
+            pre.next = list1;
+        }
+
+        if (list2 != null) {
+            pre.next = list2;
+        }
+
+        return head.next;
+
+    }
+
+
+
 
 
 
